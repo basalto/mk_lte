96405060e7f328421b34284e648c17f9f5662d81
Ricardo Dinis <basalto@gmail.com>
2015-06-25 02:03:07 +0100

    Several changes

--- ftp_dl_4g.R
+++ ftp_dl_4g.R
@@ -1,66 +1,104 @@
-#library(XLConnect)
-#library(gdata)
 library(ggplot2)
-library(scatterplot3d)
 library(dplyr)
+library(scales)
+library(gridExtra)
 
+#setwd("/home/rjdinis/development/R/mk_lte")
 setwd("/home/rjdinis/development/R/mk_lte")
-source("multiplot.R")
 
-ftp_dl_4g <- read.table("/home/rjdinis/development/R/Throughput_4G/FTP_DL_4G_All_v2.csv", header=TRUE, fill=TRUE, sep=',')
+ftp_dl_4g <- read.table("FTP_DL_4G_All_v2.csv", header=TRUE, fill=TRUE, sep=',')
 
-#wb = loadWorkbook("/home/rjdinis/development/R/Throughput_4G/FTP_DL_4G_All.xlsx")
-#df = readWorksheet(wb, sheet = "Sheet1", header = TRUE)
-#theData <- readWorksheet(loadWorkbook("/home/rjdinis/development/R/Throughput_4G/FTP_DL_4G_All.xlsx"),sheet=1)
-#df = read.xls ("/home/rjdinis/development/R/Throughput_4G/FTP_DL_4G_All.xlsx"), sheet = 1, header = TRUE)
+# Format table fields
+ftp_dl_4g$DLBandWidth <- as.factor(ftp_dl_4g$DLBandWidth)
 
 
+# Filter smaples with residual throughput
+Throughput.DL.min.filter <- 10
 ftp_dl_4g_ie <- select(ftp_dl_4g, RNC, Technology, DLBandWidth, RSRP=Serving.RSRP..dBm., RSRQ=Serving.RSRQ..dB., SNR=Serving.RS.SNR..dB., CQI=Rank1.CQI, BLER=PDSCH.BLER.Total...., Modulation=PDSCH.Modulation.CW0, MCS=AvgMCS, RB_DL=AvgRBsFrame, TM=TransmissionMode, Throughput.DL.MAC=MAC.DL.Throughput..kb.sec., Throughput.DL.RLC=RLC.DL.Throughput..kb.sec., Throughput.DL.PDCP=PDCP.DL.Throughput..kb.sec., Throughput.DL.APP=App.Throughput.DL..kbit.s.)
-ftp_dl_4g_filter <- filter(ftp_dl_4g_ie, Throughput.DL.MAC > 10)
-
-h1 <- ggplot(ftp_dl_4g_filter, aes(x=RSRP)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h2 <- ggplot(ftp_dl_4g_filter, aes(x=RSRQ)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h3 <- ggplot(ftp_dl_4g_filter, aes(x=SNR)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h4 <- ggplot(ftp_dl_4g_filter, aes(x=CQI)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-multiplot(h1, h2, h3, h4, cols=2)
-
-h5 <- ggplot(ftp_dl_4g_filter, aes(x=MCS)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h6 <- ggplot(ftp_dl_4g_filter, aes(x=TM)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h7 <- ggplot(ftp_dl_4g_filter, aes(x=Modulation)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h8 <- ggplot(ftp_dl_4g_filter, aes(x=BLER)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-multiplot(h5, h6, h7, h8, cols=2)
-
-h9 <- ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.MAC)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h10 <- ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.RLC)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h11 <- ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.PDCP)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h12 <- ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.APP)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-multiplot(h9, h10, h11, h12, cols=2)
-
-h20 <- ggplot(ftp_dl_4g, aes(x=DLBandWidth)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
-h21 <- ggplot(ftp_dl_4g_ie, aes(x=RB_DL)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red")
+ftp_dl_4g_filter <- filter(ftp_dl_4g_ie, Throughput.DL.MAC > Throughput.DL.min.filter)
 
 
-c1 <- ggplot(ftp_dl_4g_filter, aes(x=RSRP, y=RSRQ)) + geom_point() + geom_density2d()
-c2 <- ggplot(ftp_dl_4g_filter, aes(x=RSRP, y=SNR)) + geom_point() + geom_density2d()
-c3 <- ggplot(ftp_dl_4g_filter, aes(x=RSRP, y=CQI)) + geom_point() + geom_density2d()
-multiplot(c1, c2, c3, cols=3)
+# Create RF Signal Histograms
+(h1.dl <- ggplot(ftp_dl_4g_filter, aes(x=RSRP)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h2.dl <- ggplot(ftp_dl_4g_filter, aes(x=RSRQ)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h3.dl <- ggplot(ftp_dl_4g_filter, aes(x=SNR)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h4.dl <- ggplot(ftp_dl_4g_filter, aes(x=CQI)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(m1.dl <- arrangeGrob(h1.dl, h2.dl, h3.dl, h4.dl, ncol=2, nrow=2))
+(ggsave(m1.dl, file=paste0("./Charts/Histograms_RF_DL.jpeg"), width=20, height=10, dpi=100))
 
-s1 <- ggplot(ftp_dl_4g_filter, aes(x=RSRP, y=Throughput.DL.MAC)) + geom_point() + geom_density2d()
-s2 <- ggplot(ftp_dl_4g_filter, aes(x=RSRQ, y=Throughput.DL.MAC)) + geom_point() + geom_density2d()
-s3 <- ggplot(ftp_dl_4g_filter, aes(x=SNR, y=Throughput.DL.MAC)) + geom_point() + geom_density2d()
-s4 <- ggplot(ftp_dl_4g_filter, aes(x=CQI, y=Throughput.DL.MAC)) + geom_point() + geom_density2d()
-s5 <- ggplot(ftp_dl_4g_filter, aes(x=MCS, y=Throughput.DL.MAC)) + geom_point() + geom_density2d()
-s6 <- ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.MAC, y=BLER)) + geom_point() + geom_density2d()
-multiplot(s1, s2, s3, s4, s5, s6, cols=3)
 
-ggplot(ftp_dl_4g_filter, aes(x=SNR, y=Throughput.DL.MAC, cut=Modulation)) + geom_point() + geom_density2d()
-ggplot(ftp_dl_4g_ie, aes(x=SNR, y=Throughput.DL.MAC)) + geom_jitter(size=1) + facet_wrap(~ Modulation)
-ggplot(ftp_dl_4g_ie, aes(x=SNR, y=Throughput.DL.MAC)) + geom_jitter(size=1) + facet_wrap(~ DLBandWidth)
-ggplot(ftp_dl_4g_ie, aes(x=SNR, y=Throughput.DL.MAC)) + geom_jitter(size=1) + facet_wrap(~ Technology)
+# Create Scheduler Histograms
+(h5.dl <- ggplot(ftp_dl_4g_filter, aes(x=MCS)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h6.dl <- ggplot(ftp_dl_4g_filter, aes(x=factor(TM))) + geom_histogram(aes(fill = ..count..), rm.na=TRUE) + labs(x="Transmission Mode") + scale_fill_gradient("Count", low = "green", high = "red"))
+(h7.dl <- ggplot(ftp_dl_4g_filter, aes(x=Modulation)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h8.dl <- ggplot(ftp_dl_4g_ie, aes(x=RB_DL)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(m2.dl <- arrangeGrob(h5.dl, h6.dl, h7.dl, h8.dl, ncol=2, nrow=2))
+ggsave(m2.dl, file=paste0("./Charts/Histograms_Scheduler_DL.jpeg"), width=20, height=10, dpi=100)
 
-ggplot(ftp_dl_4g_ie, aes(x=RB_DL, y=Throughput.DL.MAC)) + geom_point() + geom_density2d()
-ggplot(ftp_dl_4g_filter, aes(x=SNR, y=Throughput.DL.MAC)) + geom_point() + facet_wrap(~ RNC, ncol=2)
 
-s3d <- scatterplot3d(ftp_dl_4g_ie$SNR,ftp_dl_4g_ie$RB_DL,ftp_dl_4g_ie$Throughput.DL.MAC, pch=16, highlight.3d=TRUE, type="h", main="3D Scatterplot")
-fit <- lm(ftp_dl_4g_ie$SNR ~ ftp_dl_4g_ie$RB_DL+ftp_dl_4g_ie$Throughput.DL.MAC)
-s3d$plane3d(fit)
+# Create Throughput Histograms
+(h9.dl <- ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.MAC)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h10.dl <- ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.RLC)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+#(h11.dl <- ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.PDCP)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h12.dl <- ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.APP)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h13.dl <- ggplot(ftp_dl_4g_filter, aes(x=BLER)) + geom_histogram(aes(fill = ..count..)) + scale_fill_gradient("Count", low = "green", high = "red"))
+(m3.dl <- arrangeGrob(h9.dl, h10.dl, h12.dl, h13.dl, ncol=2, nrow=2))
+ggsave(m3.dl, file=paste0("./Charts/Histograms_Throughput_DL.jpeg"), width=20, height=10, dpi=100)
+
+
+# Create Bandwidth Histograms
+(h20.dl <- ggplot(ftp_dl_4g_filter, aes(x=factor(DLBandWidth))) + geom_histogram(fill="red", rm.na=TRUE) + labs(x="Bandwidth (MHz)", y="Numeber of Samples"))
+(h21.dl <-ggplot(ftp_dl_4g_filter, aes(x = DLBandWidth), na.omit(mydataf)) + geom_bar(aes(y = (..count..)/sum(..count..)), labels = percent_format(), fill="red") + scale_y_continuous(breaks=seq(0, 1, 0.2), limits=c(0, 1)) + labs(x="Bandwidth (MHz)", y="Percentage of Samples"))
+(m4.dl <- arrangeGrob(h20.dl, h21.dl, ncol=2, nrow=1))
+ggsave(m4.dl, file=paste0("./Charts/Histograms_Bandwidth_DL.jpeg"), width=20, height=10, dpi=100)
+
+
+# Create RF Signal Scatter charts
+(c1.dl <- ggplot(ftp_dl_4g_filter, aes(x=RSRP, y=RSRQ)) + geom_point(colour="red", size=0.9))
+(c2.dl <- ggplot(ftp_dl_4g_filter, aes(x=RSRP, y=SNR)) + geom_point(colour="red", size=0.9))
+(c3.dl <- ggplot(ftp_dl_4g_filter, aes(x=RSRP, y=CQI)) + geom_point(colour="red", size=1))
+(m5.dl <- arrangeGrob(c1.dl, c2.dl, c3.dl, ncol=3, nrow=1))
+ggsave(m5.dl, file=paste0("./Charts/Scatter_RF_DL.jpeg"), width=20, height=10, dpi=100)
+
+# Create RF Thrgouhput Scatter charts
+(s1.dl <- ggplot(ftp_dl_4g_filter, aes(x=RSRP, y=Throughput.DL.MAC)) + geom_point(colour="red", size=0.9))
+(s2.dl <- ggplot(ftp_dl_4g_filter, aes(x=RSRQ, y=Throughput.DL.MAC)) + geom_point(colour="red", size=0.9))
+(s3.dl <- ggplot(ftp_dl_4g_filter, aes(x=SNR, y=Throughput.DL.MAC)) + geom_point(colour="red", size=0.9))
+(s4.dl <- ggplot(ftp_dl_4g_filter, aes(x=CQI, y=Throughput.DL.MAC)) + geom_point(colour="red", size=0.9))
+(s5.dl <- ggplot(ftp_dl_4g_filter, aes(x=MCS, y=Throughput.DL.MAC)) + geom_point(colour="red", size=0.9))
+(s6.dl <- ggplot(ftp_dl_4g_filter, aes(x=RB_DL, y=Throughput.DL.MAC)) + geom_point(colour="red", size=1))
+(m6.dl <- arrangeGrob(s1.dl, s2.dl, s3.dl, s4.dl, s5.dl, s6.dl, ncol=3, nrow=2))
+ggsave(m6.dl, file=paste0("./Charts/Scatter_Throughput_DL.jpeg"), width=20, height=10, dpi=100)
+
+
+# Create Interference Scatter charts
+(s10.dl <- ggplot(ftp_dl_4g_filter, aes(x=SNR, y=MCS)) + geom_point(colour="red", size=1))
+(s11.dl <- ggplot(ftp_dl_4g_filter, aes(x=CQI, y=SNR)) + geom_point(colour="red", size=1))
+(s12.dl <- ggplot(ftp_dl_4g_filter, aes(x=SNR, y=BLER)) + geom_point(colour="red", size=1))
+(m7.dl <- arrangeGrob(s10.dl, s11.dl, s12.dl, ncol=3, nrow=1))
+ggsave(m7.dl, file=paste0("./Charts/Scatter_Interference_DL.jpeg"), width=20, height=10, dpi=100)
+
+
+# Create Throughput Cuts
+(s20.dl <- ggplot(ftp_dl_4g_ie, aes(x=SNR, y=Throughput.DL.MAC)) + geom_jitter(colour="red", size=1) + facet_wrap(~ Modulation))
+ggsave(s20.dl, file=paste0("./Charts/Scatter_Throughput_DL_by_Modulation.jpeg"), width=20, height=10, dpi=100)
+(s21.dl <- ggplot(ftp_dl_4g_ie, aes(x=SNR, y=Throughput.DL.MAC)) + geom_jitter(colour="red", size=1) + facet_wrap(~ DLBandWidth))
+ggsave(s21.dl, file=paste0("./Charts/Scatter_Throughput_DL_by_Bandwidt.jpeg"), width=20, height=10, dpi=100)
+(s22.dl <- ggplot(ftp_dl_4g_ie, aes(x=SNR, y=Throughput.DL.MAC)) + geom_jitter(colour="red", size=1) + facet_wrap(~ Technology))
+ggsave(s.22.dl, file=paste0("./Charts/Scatter_Throughput_DL_by_Technology.jpeg"), width=20, height=10, dpi=100)
+(s23.dl <- ggplot(ftp_dl_4g_filter, aes(x=SNR, y=Throughput.DL.MAC)) + geom_point(colour="red", size=1) + facet_wrap(~ RNC, ncol=2))
+ggsave(s23.dl, file=paste0("./Charts/Scatter_Throughput_DL_by_RNC.jpeg"), width=20, height=10, dpi=100)
+
+# Other charts
+ggplot(ftp_dl_4g_filter, aes(x=Throughput.DL.MAC, y=BLER)) + geom_point(colour="red", size=0.9)
+ggplot(ftp_dl_4g_filter, aes(x=SNR, y=BLER)) + geom_point(colour="red", size=0.9)
+
+
+# Create table with count and percentage
+tblFun <- function(x){
+  tbl <- table(x)
+  res <- cbind(tbl,round(prop.table(tbl)*100,2))
+  colnames(res) <- c('Count','Percentage')
+}
+ag.DLBandwidth <- tblFun(ftp_dl_4g$DLBandWidth)
+
--- ftp_ul_4g.R
+++ ftp_ul_4g.R
@@ -1,17 +1,13 @@
-#library(XLConnect)
-#library(gdata)
 library(ggplot2)
-library(plyr)
 library(dplyr)
 library(scales)
+library(gridExtra)
 
+#setwd("/home/rjdinis/development/R/mk_lte")
 setwd("/home/rjdinis/development/R/mk_lte")
 
-# Importar function para criar multiplots
-source("multiplot.R")
-
 # Importar ficheiro com dados Raw do FTP UL 4G
-ftp_ul_4g <- read.table("/home/rjdinis/development/R/mk_lte/FTP_UL_4G_All_v2.csv", header=TRUE, fill=TRUE, sep=',')
+ftp_ul_4g <- read.table("FTP_UL_4G_All_v2.csv", header=TRUE, fill=TRUE, sep=',')
 
 # Corrige variaveis
 ftp_ul_4g$NumRBs <- ftp_ul_4g$NumRBs/1000
@@ -22,60 +18,75 @@ ftp_ul_4g$TM <- factor(ftp_ul_4g$TM)
 
 
 # Falta BLER UL, Modulation UL, MCS UL
-ftp_ul_4g_ie <- select(ftp_ul_4g, RNC, Technology, DLBandWidth, RSRP=Serving.RSRP..dBm., RSRQ=Serving.RSRQ..dB., SNR=Serving.RS.SNR..dB., CQI=Rank1.CQI, AvgMCS, RB.UL=NumRBs, PuschTxPower, TxPower=Tx.Power..dBm., RxPower=Rx.Power..dBm., Throughput.UL.MAC=MAC.UL.Throughput..kb.sec., Throughput.UL.RLC=RLC.UL.Throughput..kb.sec., Throughput.UL.PDCP=PDCP.UL.Throughput..kb.sec., Throughput.UL.APP=App.Throughput.UL..kbit.s.)
+Throughput.UL.min.filter <- 100
+ftp_ul_4g_ie <- select(ftp_ul_4g, RNC, Technology, DLBandWidth, RSRP=Serving.RSRP..dBm., RSRQ=Serving.RSRQ..dB., SNR=Serving.RS.SNR..dB., CQI=Rank1.CQI, AvgMCS, RB.UL=NumRBs, MCS.UL = AvgMCS, PuschTxPower, TxPower=Tx.Power..dBm., RxPower=Rx.Power..dBm., Throughput.UL.MAC=MAC.UL.Throughput..kb.sec., Throughput.UL.RLC=RLC.UL.Throughput..kb.sec., Throughput.UL.PDCP=PDCP.UL.Throughput..kb.sec., Throughput.UL.APP=App.Throughput.UL..kbit.s.)
+ftp_ul_4g_filter <- filter(ftp_ul_4g_ie, Throughput.UL.MAC > Throughput.UL.min.filter)
 
 
-# Filtrar amostras para remover pontos com Throughput  a 100
-ftp_ul_4g_filter <- filter(ftp_ul_4g_ie, Throughput.UL.MAC > 100)
+(h1.ul <- ggplot(ftp_ul_4g_filter, aes(x=RSRP)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h2.ul <- ggplot(ftp_ul_4g_filter, aes(x=RSRQ)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h3.ul <- ggplot(ftp_ul_4g_filter, aes(x=SNR)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h4.ul <- ggplot(ftp_ul_4g_filter, aes(x=CQI)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(m1.ul <- arrangeGrob(h1.ul, h2.ul, h3.ul, h4.ul, ncol=2, nrow=2))
+(ggsave(m1, file=paste0("./Charts/Histograms_RF_UL.jpeg"), width=20, height=10, dpi=100))
 
 
-h1 <- ggplot(ftp_ul_4g_filter, aes(x=RSRP)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h2 <- ggplot(ftp_ul_4g_filter, aes(x=RSRQ)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h3 <- ggplot(ftp_ul_4g_filter, aes(x=SNR)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h4 <- ggplot(ftp_ul_4g_filter, aes(x=CQI)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-multiplot(h1, h2, h3, h4, cols=2)
+(h5.ul <- ggplot(ftp_ul_4g_filter, aes(x=PuschTxPower)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h6.ul <- ggplot(ftp_ul_4g_filter, aes(x=TxPower)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h7.ul <- ggplot(ftp_ul_4g_filter, aes(x=RxPower)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(m2.ul <- arrangeGrob(h5.ul, h6.ul, h7.ul, ncol=3, nrow=1))
+(ggsave(m2.ul, file=paste0("./Charts/Histograms_Power_UL.jpeg"), width=20, height=10, dpi=100))
 
 
-h5 <- ggplot(ftp_ul_4g_filter, aes(x=PuschTxPower)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h6 <- ggplot(ftp_ul_4g_filter, aes(x=TxPower)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h7 <- ggplot(ftp_ul_4g_filter, aes(x=RxPower)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-multiplot(h5, h6, h7, cols=3)
-
-h9 <- ggplot(ftp_ul_4g_filter, aes(x=Throughput.UL.MAC)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h10 <- ggplot(ftp_ul_4g_filter, aes(x=Throughput.UL.RLC)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h11 <- ggplot(ftp_ul_4g_filter, aes(x=Throughput.UL.PDCP)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h12 <- ggplot(ftp_ul_4g_filter, aes(x=Throughput.UL.APP)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-multiplot(h9, h10, h12, cols=3)
+(h9.ul <- ggplot(ftp_ul_4g_filter, aes(x=Throughput.UL.MAC)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h10.ul <- ggplot(ftp_ul_4g_filter, aes(x=Throughput.UL.RLC)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+#(h11.ul <- ggplot(ftp_ul_4g_filter, aes(x=Throughput.UL.PDCP)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h12.ul <- ggplot(ftp_ul_4g_filter, aes(x=Throughput.UL.APP)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(m3.ul <- arrangeGrob(h9.ul, h10.ul, h12.ul, ncol=3, nrow=1))
+(ggsave(m3.ul, file=paste0("./Charts/Histograms_Throughput_UL.jpeg"), width=20, height=10, dpi=100))
 
 
-h13 <- ggplot(ftp_ul_4g_filter, aes(x=RB.UL)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h14 <- ggplot(ftp_ul_4g_filter, aes(x=AvgMCS)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red")
-h15 <- ggplot(ftp_ul_4g) +  geom_bar(aes(x=DLBandWidth), fill = "red")
-multiplot(h13, h14, h15, cols=3)
+(h13.ul <- ggplot(ftp_ul_4g_filter, aes(x=RB.UL)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(h14.ul <- ggplot(ftp_ul_4g_filter, aes(x=MCS.UL)) + geom_histogram(aes(fill = ..count..), na.rm = TRUE) + scale_fill_gradient("Count", low = "green", high = "red"))
+(m4.ul <- arrangeGrob(h13.ul, h14, ncol=2, nrow=1))
+(ggsave(m4.ul, file=paste0("./Charts/Histograms_Scheduler_UL.jpeg"), width=20, height=10, dpi=100))
 
 
-c1 <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=RSRQ)) + geom_point(na.rm = TRUE) + geom_density2d()
-c2 <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=SNR)) + geom_point(na.rm = TRUE) + geom_density2d()
-c3 <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=CQI)) + geom_point(na.rm = TRUE) + geom_density2d()
-c4 <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=PuschTxPower)) + geom_point(na.rm = TRUE) + geom_density2d()
-multiplot(c1, c2, c3, c4, cols=2)
+# Create Bandwidth Histograms
+(h15.ul <- ggplot(ftp_ul_4g_filter, aes(x=factor(DLBandWidth))) + geom_histogram(fill="red", rm.na=TRUE) + labs(x="Bandwidth (MHz)", y="Numeber of Samples"))
+(h16.ul <-ggplot(ftp_ul_4g_filter, aes(x = DLBandWidth), na.omit(mydataf)) + geom_bar(aes(y = (..count..)/sum(..count..)), labels = percent_format(), fill="red") + scale_y_continuous(breaks=seq(0, 1, 0.2), limits=c(0, 1)) + labs(x="Bandwidth (MHz)", y="Percentage of Samples"))
+(m5.ul <- arrangeGrob(h15.ul, h16.ul, ncol=2, nrow=1))
+ggsave(m5.ul, file=paste0("./Charts/Histograms_Bandwidth_UL.jpeg"), width=20, height=10, dpi=100)
 
-s1 <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=Throughput.UL.MAC)) + geom_point(na.rm = TRUE) + geom_density2d()
-s2 <- ggplot(ftp_ul_4g_filter, aes(x=RSRQ, y=Throughput.UL.MAC)) + geom_point(na.rm = TRUE) + geom_density2d()
-s3 <- ggplot(ftp_ul_4g_filter, aes(x=SNR, y=Throughput.UL.MAC)) + geom_point(na.rm = TRUE) + geom_density2d()
-s4 <- ggplot(ftp_ul_4g_filter, aes(x=PuschTxPower, y=Throughput.UL.MAC)) + geom_jitter(na.rm = TRUE)
-multiplot(s1, s2, s3, s4, cols=2)
 
+(c1.ul <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=RSRQ)) + geom_point(colour="red", size=0.9, na.rm = TRUE))
+(c2.ul <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=SNR)) + geom_point(colour="red", size=0.9, na.rm = TRUE))
+(c3.ul <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=CQI)) + geom_point(colour="red", size=0.9, na.rm = TRUE))
+(c4.ul <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=PuschTxPower)) + geom_point(colour="red", size=0.9, na.rm = TRUE))
+(m6.ul <- arrangeGrob(c1.ul, c2.ul, c3.ul, c4.ul, ncol=2, nrow=2))
+(ggsave(m6.ul, file=paste0("./Charts/Scatter_RF_UL.jpeg"), width=20, height=10, dpi=100))
+
+
+(s1.ul <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=Throughput.UL.MAC)) + geom_point(colour="red", size=0.9, na.rm = TRUE))
+(s2.ul <- ggplot(ftp_ul_4g_filter, aes(x=RSRQ, y=Throughput.UL.MAC)) + geom_point(colour="red", size=0.9, na.rm = TRUE))
+(s3.ul <- ggplot(ftp_ul_4g_filter, aes(x=SNR, y=Throughput.UL.MAC)) + geom_point(colour="red", size=0.9, na.rm = TRUE))
+(s4.ul <- ggplot(ftp_ul_4g_filter, aes(x=PuschTxPower, y=Throughput.UL.MAC)) + geom_jitter(colour="red", size=0.9, na.rm = TRUE))
+(m7.ul <- arrangeGrob(s1.ul, s2.ul, s3.ul, s4.ul, ncol=2, nrow=2))
+(ggsave(m7.ul, file=paste0("./Charts/Scatter_Throughput_UL.jpeg"), width=20, height=10, dpi=100))
+
+
+# Create Throughput cuts
+(s5.ul <- ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=Throughput.UL.MAC)) + geom_point(colour="red", size=1) + facet_wrap(~ RNC, ncol=2))
+(ggsave(s5.ul, file=paste0("./Charts/Scatter_Throughput_UL_by_RNC.jpeg"), width=20, height=10, dpi=100))
+
+
+# Other charts
 ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=Throughput.UL.MAC, colour=Throughput.UL.MAC)) + geom_jitter(size=1.5, na.rm = TRUE) + facet_wrap(~ DLBandWidth)
 ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=Throughput.UL.MAC, colour="red")) + geom_jitter(size=1.5, na.rm = TRUE) + facet_wrap(~ Technology) + guides(colour=FALSE)
 
 ggplot(ftp_ul_4g_filter, aes(x=RSRP, y=Throughput.UL.MAC, colour="red")) + geom_jitter(na.rm = TRUE) + facet_wrap(~ RNC, ncol=2) + guides(colour=FALSE)
 #ggsave(m1, file="Throughput.jpeg", path="/home/rjdinis/development/R/Throughput_4G/Charts")
 
-
-DLBandwidth_label <- ddply(ftp_ul_4g, .(DLBandWidth), summarize, y=length(DLBandWidth))
-ggplot(ftp_ul_4g) +
-  geom_bar(aes(x=DLBandWidth), fill = "red")
 
 #+ scale_y_continuous(labels = percent_format())
 #+ geom_text(aes(label = DLBandWidth_label))
